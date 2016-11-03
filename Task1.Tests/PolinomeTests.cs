666b4750be5512d0b73fb717d606d3c46239c42d
Masha <Masha>
2016-11-03 19:23:22 +0300

    Initial commit

---
+++ Task1.Tests/PolinomeTests.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using NUnit.Framework;
+
+namespace Task1.Tests
+{
+    [TestFixture]
+    public class PolinomeTests
+    {
+        private static Polinome source1 = 
+            new Polinome(1.2, 6.2345, 541.001, 2.1484, -0.05, 0.1, -2.11);
+        private static Polinome source2 =
+            new Polinome(3.8, 15.333679, -2.145);
+        private static Polinome expectedSum = 
+            new Polinome(1.2, 6.2345, 541.001, 2.1484, 3.75, 15.433679, -4.255);
+        private static Polinome expectedDif1 =
+            new Polinome(1.2, 6.2345, 541.001, 2.1484, -3.85, -15.233679, 0.035);
+        private static Polinome expectedDif2 =
+            new Polinome(-1.2, -6.2345, -541.001, -2.1484, 3.85, 15.233679, -0.035);
+        private static Polinome expectedMul =
+            new Polinome(4.56, 42.0915148, 2148.8276217255, 8290.326590179, 
+                -1127.6942690364, -4.99500195, -6.3773821, -32.56856269, 4.52595);
+
+        [Test, TestCaseSource(typeof(PolinomeSumTest), nameof(PolinomeSumTest.TestCasesSum))]
+        public Polinome SumTests(Polinome p1, Polinome p2)
+        {
+            return p1 + p2;
+        }
+
+        [Test, TestCaseSource(typeof(PolinomeDifTest), nameof(PolinomeDifTest.TestCasesDif))]
+        public Polinome DifTests(Polinome p1, Polinome p2)
+        {
+            return p1 - p2;
+        }
+
+        [Test, TestCaseSource(typeof(PolinomeMulTest), nameof(PolinomeMulTest.TestCasesMul))]
+        public Polinome MulTests(Polinome p1, Polinome p2)
+        {
+            return p1 * p2;
+        }
+
+        public class PolinomeSumTest
+        {
+            public static IEnumerable TestCasesSum
+            {
+                get
+                {
+                    yield return new TestCaseData(source1, source2).Returns(expectedSum);
+                    yield return new TestCaseData(source2, source1).Returns(expectedSum);
+                }
+            }
+        }
+
+        public class PolinomeDifTest
+        {
+            public static IEnumerable TestCasesDif
+            {
+                get
+                {
+                    yield return new TestCaseData(source1, source2).Returns(expectedDif1);
+                    yield return new TestCaseData(source2, source1).Returns(expectedDif2);
+                }
+            }
+        }
+        public class PolinomeMulTest
+        {
+            public static IEnumerable TestCasesMul
+            {
+                get
+                {
+                    yield return new TestCaseData(source1, source2).Returns(expectedMul);
+                    yield return new TestCaseData(source2, source1).Returns(expectedMul);
+                }
+            }
+        }
+    }
+}
---
+++ Task1/Polinome.cs
@@ -0,0 +1,170 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Polinome
+    {
+        private readonly double[] arr;
+        private readonly int degree;
+
+        public Polinome(params double[] data)
+        {
+            if (data == null)
+            {
+                throw new NullReferenceException("NULL data!");
+            }
+
+            if (data.Length < 1)
+            {
+                throw new ArgumentException("Wrong number of parameters!");
+            }
+
+            degree = data.Length - 1;
+            arr = new double[degree + 1];
+
+            data.CopyTo(arr, 0);
+        }
+
+        public Polinome(int degree)
+        {
+            if (degree < 0)
+            {
+                throw new ArgumentOutOfRangeException("Wrong polinome degree!");
+            }
+
+            this.degree = degree;
+            arr = new double[degree + 1];
+        }
+
+        public Polinome(Polinome p)
+        {
+            degree = p.degree;
+            p.arr.CopyTo(arr, 0);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var p = obj as Polinome;
+
+            if (degree != p?.degree)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < degree + 1; i++)
+            {
+                if (Math.Abs(arr[i] - (p.arr[i])) > Math.Pow(10, -6))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool Equals(Polinome p)
+        {
+            if (p == null)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < degree + 1; i++)
+            {
+                if (!arr[i].Equals(p.arr[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return degree * 0x00010000 + 13;
+        }
+
+        public static Polinome operator -(Polinome p)
+        {
+            var newArr = new double[p.degree + 1];
+
+            for (int i = 0; i < p.degree + 1; i++)
+            {
+                newArr[i] = -p.arr[i];
+            }
+
+            return new Polinome(newArr);
+        }
+
+
+        public static Polinome operator +(Polinome p1, Polinome p2)
+        {
+            int maxDegree = p1.degree > p2.degree ? p1.degree : p2.degree;
+            int minDegree = p1.degree < p2.degree ? p1.degree : p2.degree;
+            var greatestPolinome = maxDegree == p1.degree ? p1 : p2;
+            var lesserPolinome = minDegree == p1.degree ? p1 : p2;
+            var newPolinome = greatestPolinome;
+
+            for (int i = newPolinome.degree, j = minDegree; i > minDegree + 1; i--, j--)
+            {
+                greatestPolinome.arr[i] += lesserPolinome.arr[j];
+            }
+
+            return newPolinome;
+        }
+
+        public static Polinome operator -(Polinome p1, Polinome p2)
+        {
+            int maxDegree = p1.degree > p2.degree ? p1.degree : p2.degree;
+            int minDegree = p1.degree < p2.degree ? p1.degree : p2.degree;
+            var greatestPolinome = maxDegree == p1.degree ? p1 : -p2;
+            var lesserPolinome = minDegree == p1.degree ? p1 : -p2;
+            var newPolinome = greatestPolinome;
+
+            for (int i = newPolinome.degree, j = minDegree; i > minDegree + 1; i--, j--)
+            {
+                greatestPolinome.arr[i] += lesserPolinome.arr[j];
+            }
+
+            return newPolinome;
+        }
+
+        public static Polinome operator *(Polinome p1, Polinome p2)
+        {
+            var newPolinome = new Polinome(p1.degree + p2.degree);
+
+            for (int i = 0; i < newPolinome.degree + 1; i++)
+            {
+                newPolinome.arr[i] = Coefficient(p1.arr, p2.arr, newPolinome.degree - i);
+            }
+
+            return newPolinome;
+        }
+
+        /// <summary>
+        /// Counts a coefficient of the specified degree.
+        /// </summary>
+        /// <param name="arr1">Coefficients of the first polinome.</param>
+        /// <param name="arr2">Coefficients of the second polinome.</param>
+        /// <param name="degree">A degree.</param>
+        /// <returns>Return a coefficient.</returns>
+        private static double Coefficient(double[] arr1, double[] arr2, int degree)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                for (int j = 0; j < arr2.Length; j++)
+                {
+                    if (i + j == (arr1.Length - 1) + (arr2.Length - 1) - degree)
+                    {
+                        sum += arr1[i]*arr2[j];
+                    }
+                }
+            }
+
+            return sum;
+        }
+    }
+}
